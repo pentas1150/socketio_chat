0063db0a753cc73f5a8d8dff8ed12856b730f2e4
pentas1150 <beewol@soongsil.ac.kr>
2020-02-09 18:22:55 +0900

    1)chat.ejs에서 socketio 연결 도메인 동적 설정(테스트와 배포 환경 편의성 위해)

--- routes/chat.ts
+++ routes/chat.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { Room } from "../models/Room";
 import { isNotLoggedIn } from "./middleware";
 const router = Router();
+require("dotenv").config();
 
 router.get("/:id", isNotLoggedIn, async (req, res, next) => {
   const room = await Room.findOne({ where: { id: req.params.id } });
@@ -10,7 +11,10 @@ router.get("/:id", isNotLoggedIn, async (req, res, next) => {
       "<script>alert('존재하지 않는 방입니다.'); window.location.href='/main';</script>"
     );
   }
-  return res.render("chat", { myId: req.session.passport.user });
+  return res.render("chat", {
+    myId: req.session.passport.user,
+    domain: process.env.DOMAIN
+  });
 });
 
 module.exports = router;
