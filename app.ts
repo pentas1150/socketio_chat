25a0c0743d0390ffaeac745f1165aaead63e7808
pentas1150 <beewol@soongsil.ac.kr>
2020-02-13 15:33:44 +0900

    1)room.ts 삭제 및 chat.ts로 병합

--- app.ts
+++ app.ts
@@ -49,7 +49,6 @@ app.use(cors());
 app.use("/", require("./routes/index"));
 app.use("/main", require("./routes/main"));
 app.use("/login", require("./routes/login"));
-//app.use("/room", require("./routes/room"));
 app.use("/chat", require("./routes/chat"));
 app.use("/upload", require("./routes/upload"));
 
